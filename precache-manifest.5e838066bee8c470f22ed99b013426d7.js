6ad6ec083b777dbee1b438f3664ab7073d3a97ed
Raphael Barone <rpbarone.dev@gmail.com>
2020-05-07 04:16:12 -0300

    Updates

---
+++ precache-manifest.5e838066bee8c470f22ed99b013426d7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6d1b56af93373b234a78cb78f05d849",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "3a2acc34e2ad96816477",
+    "url": "/monsters-rolodex/static/css/main.2309ca4d.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3a2acc34e2ad96816477",
+    "url": "/monsters-rolodex/static/js/main.f02bf6b6.chunk.js"
+  },
+  {
+    "revision": "f051cecc4ee3cb3b301f",
+    "url": "/monsters-rolodex/static/js/runtime-main.eea62ddf.js"
+  }
+]);
